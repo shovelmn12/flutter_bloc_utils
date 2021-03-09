74bfeb64121c22f699d7aed9c9d51161568d5c0b
Shovel Maman <shovelmn12@gmail.com>
2021-03-09 12:11:39 +0100

    defs fixes

--- lib/actions/defs.dart
+++ lib/actions/defs.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-typedef BlocActionCallback<T extends Cubit<S>, S> = void Function<T, S>(
-    BuildContext, T);
+typedef BlocActionCallback<C extends Cubit<S>, S> = void Function(
+    BuildContext context, C bloc);
