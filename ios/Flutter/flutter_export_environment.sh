69cb29007f70e23d08d5f93f3130683bd191b547
Shovel Maman <shovelmn12@gmail.com>
2021-07-14 11:10:16 +0200

    MISC

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/shovelmn/Applications/Developer/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/shovelmn/Projects/flutter_bloc_utils"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
